3ab8ce8d9feec6d44ccac17afbbbd1d70c041d13
ksangheon <ksangheon@gmail.com>
2022-05-31 10:51:28 +0900

    2022-05-31 1rd update

---
+++ src/main/java/me/light/model/Board.java
@@ -0,0 +1,25 @@
+package me.light.model;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Board {
+	private Long bno;
+	private String title;
+	private String contents;
+	private String writer;
+	private LocalDateTime regDate;
+	private LocalDateTime updateDate;
+	
+
+}
---
+++ src/test/java/me/light/mapper/BoardMapperTest.java
@@ -0,0 +1,13 @@
+package me.light.mapper;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BoardMapperTest {
+
+	@Test
+	public void test() {
+	}
+
+}
