1e74c3fcc4f85a91a197a9631ef0c8ca3b470038
ksangheon <ksangheon@gmail.com>
2022-05-31 12:34:52 +0900

    2022-05-31 2rd update

--- src/test/java/me/light/mapper/BoardMapperTest.java
+++ src/test/java/me/light/mapper/BoardMapperTest.java
@@ -2,12 +2,21 @@ package me.light.mapper;
 
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+import java.util.List;
 
-public class BoardMapperTest {
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import me.light.config.AppTest;
+import me.light.model.Board;
+
+public class BoardMapperTest extends AppTest {
+	@Autowired
+	BoardMapper mapper;
 
 	@Test
-	public void test() {
+	public void geListTest() {
+		List<Board> list = mapper.getList();
+		assertEquals(5, list.size());
 	}
-
 }
